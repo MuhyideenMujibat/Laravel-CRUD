d954e08df46f426f66222662c7dd7891ce94ab6c
Muhyideen Mujibat <96301638+MuhyideenMujibat@users.noreply.github.com>
2023-11-15 03:59:39 +0100

    Create edit.blade.php

---
+++ edit.blade.php
@@ -0,0 +1,17 @@
+<h1> Edit Member </h1>
+
+<form action="/edit" method="POST"> 
+@csrf
+<input type="hidden" name="id" value="{{$data['id']}}"  placeholder="Name"/>
+
+<input type="text" name="fullname" value="{{$data['Name']}}"  placeholder="Name"/></br>
+</br>
+
+<input type="password" name="password" value="{{$data['Password']}}" placeholder="Password"/></br>
+</br>
+
+<input type="email" name="email" value="{{$data['Email']}}" placeholder="Email"/></br>
+</br>
+
+<button type="submit"> Update </button>
+</form>
